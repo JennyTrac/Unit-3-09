a8532acff50669b280cac4fc64ef3aa6ad7681f4
Jenny Trac <jennifer.trac@ocsbstudent.ca>
2017-10-24 13:03:25 -0400

    Initial commit

---
+++ unicode_alphabet_squared.py
@@ -0,0 +1,14 @@
+# Created by Jenny Trac
+# Created on Oct 24 2017
+# Prints all possibilities for letter pairings of the alphabet
+
+# constants and variables
+LETTER_A = 0101
+uppercase_counter = LETTER_A
+LETTER_a = 0141
+lowercase_counter = LETTER_a
+
+for uppercase_counter in range(LETTER_A, LETTER_A + 26):
+    for lowercase_counter in range(LETTER_a, LETTER_a + 26):
+        letter_combination = unichr(uppercase_counter) + unichr(lowercase_counter)
+        print(letter_combination)
